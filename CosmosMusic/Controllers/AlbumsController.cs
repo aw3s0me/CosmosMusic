85e63b5b23647716cc415c06978c59383554fd7c
Alex <aw3s0me@rocketmail.com>
2014-01-18 20:05:22 +0400

    Added entity model with models

--- CosmosMusic/Controllers/AlbumsController.cs
+++ CosmosMusic/Controllers/AlbumsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CosmosMusic.Models;
 
 namespace CosmosMusic2.Controllers
 {
@@ -11,9 +12,21 @@ namespace CosmosMusic2.Controllers
         //
         // GET: /Album/
 
+        private readonly korovin_idzEntities AlbumsContext = new korovin_idzEntities();
+
         public ActionResult Index()
         {
-            return View();
+            var genres = AlbumsContext.Genre.ToList();
+
+            return View(genres);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreEntity = AlbumsContext.Genre.FirstOrDefault(p => p.name == genre);
+            var albums = AlbumsContext.Artists.Where(p => p.Genre == genreEntity).ToList();
+            
+            return View(albums);
         }
 
     }
